6e395ffe5994f87878d68f3ffdbe4e7a0fe11bb9
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-01-10 21:34:16 +0800

    Enter raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, Read};
+
+use crossterm::terminal;
+
 fn main() {
-    println!("Hello, world!");
+    terminal::enable_raw_mode().expect("able to enable raw mode");
+    io::stdin().bytes().any(|byte| {
+        let ch = byte.expect("ascii character") as char;
+        println!("{}", ch);
+
+        let quit = ch == 'q';
+
+        if quit {
+            terminal::disable_raw_mode().expect("able to disable raw mode");
+        }
+
+        quit
+    });
 }
