0780cbc579c6becbb77e3fe9d8751ffa61316efe
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-01-12 10:42:28 +0800

    feat: show editor on a clear screen

--- src/editor.rs
+++ src/editor.rs
@@ -1,7 +1,28 @@
+use std::io;
+
+use anyhow::Result;
 use crossterm::{
     event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
-    terminal,
+    execute,
+    terminal::{self, Clear, ClearType},
 };
+
+fn clear_screen() -> Result<()> {
+    execute!(io::stdout(), Clear(ClearType::All))?;
+    Ok(())
+}
+
+fn init_terminal() -> Result<()> {
+    terminal::enable_raw_mode()?;
+    clear_screen()?;
+    Ok(())
+}
+
+fn end_terminal() -> Result<()> {
+    clear_screen()?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
 
 pub struct Editor {
     should_quit: bool,
@@ -13,35 +34,33 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("{err:#?}");
-        }
-        print!("Goodbye.\r\n");
+        init_terminal().expect("able to initialize terminal");
+
+        let repl_result = self.repl();
+
+        end_terminal().expect("able to deinit terminal");
+        repl_result.expect("repl has no fatal error");
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
-        terminal::enable_raw_mode()?;
-
+    fn repl(&mut self) -> Result<()> {
         while !self.should_quit {
-            if let Event::Key(KeyEvent {
-                code,
-                modifiers,
-                kind,
-                state,
-            }) = event::read()?
-            {
-                println!("{code:?} {modifiers:?} {kind:?} {state:?} \r");
+            let event = event::read()?;
+            self.handle_event(&event);
+        }
+        Ok(())
+    }
 
-                if matches!(
-                    (code, modifiers),
-                    (KeyCode::Char('q'), KeyModifiers::CONTROL)
-                ) {
-                    self.should_quit = true;
-                }
+    fn handle_event(&mut self, event: &Event) {
+        if let Event::Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            if matches!(
+                (code, modifiers),
+                (&KeyCode::Char('q'), &KeyModifiers::CONTROL)
+            ) {
+                self.should_quit = true;
             }
         }
-
-        terminal::disable_raw_mode()?;
-        Ok(())
     }
 }
