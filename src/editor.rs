b07d7e00b284766e6c4b4967b66163c0f5225d4a
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-01-12 10:12:18 +0800

    feat: use crossterm to read keystrokes instead

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
-use std::io::{self, Read};
-
-use crossterm::terminal;
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal,
+};
 
 pub struct Editor;
 
@@ -8,22 +9,21 @@ impl Editor {
     pub fn run(&self) {
         terminal::enable_raw_mode().expect("able to enable raw mode");
 
-        io::stdin().bytes().any(|byte| {
-            let byte = byte.expect("successful read");
-            let ch = byte as char;
+        loop {
+            match event::read() {
+                Ok(Event::Key(event)) => {
+                    println!("{:?} \r", event);
 
-            if ch.is_control() {
-                println!("Binary: {0:08b} ASCII: {0:#03} \r", byte);
-            } else {
-                println!(
-                    "Binary: {0:08b} ASCII: {0:#03} Character: {1:#?} \r",
-                    byte, ch
-                );
+                    if let KeyCode::Char(ch) = event.code {
+                        if ch == 'q' {
+                            break;
+                        }
+                    }
+                }
+                Err(err) => eprintln!("Error: {}", err),
+                _ => {}
             }
-
-            let quit = ch == 'q';
-            quit
-        });
+        }
 
         terminal::disable_raw_mode().expect("able to disable raw mode");
     }
