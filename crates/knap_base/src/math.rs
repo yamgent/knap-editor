706538684c3b9de3e50eeb65a0ad29d56102fa83
Maurice Wangleng Tan <tanwangleng@outlook.com>
2025-02-10 22:51:14 +0800

    chore: remove unused methods

--- crates/knap_base/src/math.rs
+++ crates/knap_base/src/math.rs
@@ -22,12 +22,6 @@ impl Vec2f {
     pub const ZERO: Self = Self { x: 0.0, y: 0.0 };
 }
 
-#[derive(Default, Clone, Copy, PartialEq, Eq)]
-pub struct Bounds2u {
-    pub pos: Vec2u,
-    pub size: Vec2u,
-}
-
 #[derive(Default, Clone, Copy, PartialEq)]
 pub struct Bounds2f {
     pub pos: Vec2f,
@@ -40,21 +34,6 @@ impl Bounds2f {
         size: Vec2f::ZERO,
     };
 }
-
-pub trait ToU16Clamp
-where
-    Self: TryInto<u16>,
-{
-    /// This is used to get rid of the `clippy::cast_possible_truncation`
-    /// lint error, as `Self` may have more bits than `u16`. When that happens,
-    /// then we clamp the value to `u16::MAX`
-    fn to_u16_clamp(self) -> u16 {
-        self.try_into().unwrap_or(u16::MAX)
-    }
-}
-
-impl ToU16Clamp for u64 {}
-impl ToU16Clamp for usize {}
 
 pub fn f64_to_u16_clamp(value: f64) -> u16 {
     if value.is_finite() {
